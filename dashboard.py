13c27d84272d326a99d761aaf181578df699578c
Guillaume-dcs <59201872+Guillaume-dcs@users.noreply.github.com>
2023-12-27 19:26:42 +0100

    code

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,3 @@
-from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 from Prices import EntsoeData
@@ -8,12 +7,9 @@ day = EntsoeData().start
 df = df.set_index("name")
 
 fig = px.choropleth(df, geojson=df.geometry, locations=df.index, color="DAH", 
-                    color_continuous_scale="Reds")
-fig.add_scattergeo(
-    locations=df.index, 
-    text=df.DAH,
-    mode='text')
+                    color_continuous_scale="Reds", hover_name=df.DAH)
+labels = px.scatter_geo(df, geojson=df.geometry, locations=df.index, text=df.DAH, opacity=0)
+fig.add_trace(labels.data[0])
 fig.update_geos(fitbounds="locations", visible=True)
 fig.update_layout(title_text = "DAH results {}".format(day.strftime("%Y/%m/%d")))
-
 fig.show()
