250049f0ee420dd6b31750c3fc9eca3e0131c75f
Guillaume-dcs <59201872+Guillaume-dcs@users.noreply.github.com>
2023-12-31 19:15:59 +0100

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,7 +1,9 @@
 import yfinance as yf
 import pandas as pd
-
+import numpy as np
+from sklearn.cluster import KMeans
 from ENTSOE_data import EntsoeData
+import matplotlib.pyplot as plt
 
 entsoe = EntsoeData()
 start = entsoe.start
@@ -22,21 +24,68 @@ front_coal_ticker = "MTF={}".format(get_front_month_ticker(start))
 front_coal = yf.download(front_coal_ticker, start=start-pd.DateOffset(months=1), end=start, progress=False)["Open"].round(2)
 eua = yf.download("CO2.L", start=start-pd.DateOffset(months=1), end=start, progress=False)["Open"].round(2)
 DAH_NL = entsoe.client.query_day_ahead_prices("NL", start=start-pd.DateOffset(months=1), end=end)[:-1]
-# load = entsoe.client.query_load_forecast("NL", start=start, end=end)
-# df = pd.concat([DAH_NL, load], axis=1).dropna()
-# df.columns = ["DAH_NL", "load"]
-df = pd.DataFrame(DAH_NL, columns=["PW_NL_DAH"])
+ren = entsoe.client.query_wind_and_solar_forecast("NL", start=start-pd.DateOffset(months=1), end=end, psr_type=None).sum(axis=1)
+gen = entsoe.client.query_generation("NL", start=start-pd.DateOffset(months=1), end=end)
+load = entsoe.client.query_load_forecast("NL", start=start-pd.DateOffset(months=1), end=end)
+gen_NG = gen['Fossil Gas']['Actual Aggregated']
+gen_coal = gen['Fossil Hard coal']['Actual Aggregated']
+gen_ren = gen['Wind Offshore']['Actual Aggregated'] + gen['Wind Onshore']['Actual Aggregated'] + gen['Solar']['Actual Aggregated']
+df = pd.concat([DAH_NL, gen_NG, gen_coal, gen_ren, ren, load], axis=1).dropna()
 df.index = df.index - pd.Timedelta("1 day")
 front_TTF.index = front_TTF.index.tz_localize(tz="Europe/Amsterdam")
 front_coal.index = front_coal.index.tz_localize(tz="Europe/Amsterdam")
 eua.index = eua.index.tz_localize(tz="Europe/Amsterdam")
 df = pd.concat([df, front_TTF, front_coal, eua], axis=1).ffill()
 df = df.dropna()
-df.columns = ["PW_NL_DAH", "TTF", "Coal", "EUA"]
+df.columns = ["PW_NL_DAH", "gen_NG_D+1", "gen_coal_D+1", "gen_ren_D+1", "ren", "load", "TTF", "Coal", "EUA"]
+
 df["Clean_TTF"] = (df["TTF"] + 0.185 * df["EUA"]).round(2)
-ratio = (df["Clean_TTF"] / df["PW_NL_DAH"]).round(2)
-ratio = ratio[(ratio >= 0) & (ratio <=1)]
-print(ratio)
+ratio_TTF = (df["Clean_TTF"] / df["PW_NL_DAH"]).round(2)
+ratio_TTF = ratio_TTF[(ratio_TTF >= 0) & (ratio_TTF <=1)]
+train_set_TTF = ratio_TTF.values[:-100].reshape(-1, 1)
+test_set = ratio_TTF.values[100:].reshape(-1, 1)
+kmeans_TTF = KMeans(10).fit(train_set_TTF)
+centers_TTF = kmeans_TTF.cluster_centers_.round(2)
+unique, counts = np.unique(kmeans_TTF.labels_, return_counts=True)
+frequency_TTF = dict()
+for item in range(len(centers_TTF)):
+    frequency_TTF[centers_TTF[item][0]] = counts[item] / sum(counts)
+frequency_TTF = pd.DataFrame.from_dict(frequency_TTF, orient="index")
+frequency_TTF.columns = ["frequency"]
+ng_efficiencies = list(frequency_TTF.nlargest(10, "frequency").index)
+
+df["Clean_coal"] = (df["Coal"] / 8.141 + 0.340 * df["EUA"]).round(2)
+ratio_coal = (df["Clean_coal"] / df["PW_NL_DAH"]).round(2)
+ratio_coal = ratio_coal[(ratio_coal >= 0) & (ratio_coal <=1)]
+train_set = ratio_coal.values[:-100].reshape(-1, 1)
+test_set = ratio_coal.values[100:].reshape(-1, 1)
+kmeans_coal = KMeans(10).fit(train_set)
+centers_coal = kmeans_coal.cluster_centers_.round(2)
+unique, counts = np.unique(kmeans_coal.labels_, return_counts=True)
+frequency_coal = dict()
+for item in range(len(centers_coal)):
+    frequency_coal[centers_coal[item][0]] = counts[item] / sum(counts)
+frequency_coal = pd.DataFrame.from_dict(frequency_coal, orient="index")
+frequency_coal.columns = ["frequency"]
+coal_efficiencies = list(frequency_coal.nlargest(10, "frequency").index)
+
+ng_capacities = dict()
+for ng_efficiency in ng_efficiencies:
+    df["css_{}".format(ng_efficiency)] = df["PW_NL_DAH"] - df["Clean_TTF"] / ng_efficiency
+    css_positive = df[df["css_{}".format(ng_efficiency)] >= 0]["css_{}".format(ng_efficiency)]
+    ng_capacities[ng_efficiency] = float(df[df["css_{}".format(ng_efficiency)] == css_positive.min()]["gen_NG_D+1"])
+ng_capacities = pd.DataFrame.from_dict(ng_capacities, orient="index").sort_index()
+
+coal_capacities = dict()
+for coal_efficiency in coal_efficiencies:
+    df["cds_{}".format(coal_efficiency)] = df["PW_NL_DAH"] - df["Clean_coal"] / coal_efficiency
+    cds_positive = df[df["cds_{}".format(coal_efficiency)] >= 0]["cds_{}".format(coal_efficiency)]
+    coal_capacities[coal_efficiency] = float(df[df["cds_{}".format(coal_efficiency)] == cds_positive.min()]["gen_coal_D+1"])
+coal_capacities = pd.DataFrame.from_dict(coal_capacities, orient="index").sort_index()
+
+
+
+print(ratio_TTF)
 
 # extract 3 years of hourly PW DAH prices + daily (spot or front month otherwise) open TTF EUA and Coal
 # plot histogram for each hour PW DAH / Clean TTF
